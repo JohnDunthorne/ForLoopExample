d1074c4f1afba0a99ff388944ab97ee21a0bacca
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-05 14:38:42 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+prices = [10, 20, 30]
+item = sum(prices)
+print(f"total: {item}")
+
+prices = [10, 20, 30]
+total = 0
+for price in prices:
+    total += price
+print(f"Total: {total}")
+# ^^this is a formatted string, you can use it to mix str with variables
+# it has an f before the brackets
+
+
+
